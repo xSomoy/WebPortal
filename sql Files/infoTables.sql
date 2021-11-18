aa80e3dcc3046f531e35c338aaaa39301ddaa756
John Deadman <xsomoy@tutanota.com>
2021-11-18 12:44:56 +0600

    database script updated. added status colum

--- sql Files/infoTables.sql
+++ sql Files/infoTables.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `clientinfo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `phone` varchar(15) NOT NULL,
+  `phone` varchar(15) NOT NULL,f
   `address` varchar(255) NOT NULL,
   `note` varchar(1000) NOT NULL,
   `status` varchar(10) NOT NULL,
